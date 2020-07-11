2f76d2e4fb70499eeca1871c9dac1c05e7559ca2
Emory Al-Imam <emorya@gmail.com>
2020-07-10 18:26:46 -0700

    Add RKValueTransformers

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "MGSwipeTableCell", targets: ["MGSwipeTableCell"]),
         .library(name: "RestKit", targets: ["RestKit"]),
         .library(name: "Shimmer", targets: ["Shimmer"]),
-//        .library(name: "AFNetworking", targets: ["AFNetworking"])
+        .library(name: "RKValueTransformers", targets: ["RKValueTransformers"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,6 +20,6 @@ let package = Package(
         .binaryTarget(name: "MGSwipeTableCell", url: "https://www.yapstudios.com/frameworks/MGSwipeTableCell.xcframework.zip", checksum: "2ecacc092c948dfcec93e1e4c73cb472029fc675546302905919f36100339b7d"),
         .binaryTarget(name: "RestKit", url: "https://www.yapstudios.com/frameworks/RestKit.xcframework.zip", checksum: "b83d47ea9902ddc202a78d053ad3000eae180bfb2a5b394f49da05b00984209e"),
         .binaryTarget(name: "Shimmer", url: "https://www.yapstudios.com/frameworks/Shimmer.xcframework.zip", checksum: "f1cf83ac85cb49d82bb430f849ec823e31cf8d1e4d93c5e720641a9cf941244a"),
-//        .binaryTarget(name: "AFNetworking", url: "https://www.yapstudios.com/frameworks/AFNetworking.xcframework.zip", checksum: "159e8cef77333f90e4d60f534d6e975333d597b232f01a45ef34a61b499d98ac")
+        .binaryTarget(name: "RKValueTransformers", url: "https://www.yapstudios.com/frameworks/RKValueTransformers.xcframework.zip", checksum: "f7cbff059a72dd76f76957ae729a047c0c5f71c52d77426131170c29a0b9ec3a"),
     ]
 )
